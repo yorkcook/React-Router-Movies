ce9a865069d076d35ff11fd3ee2e7155dad0b1aa
York Cook <yorkcook@Johnny-Castle.local>
2019-06-11 16:23:44 -0400

    WIP: home button working

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 export default class Movie extends Component {
   constructor(props) {
@@ -11,7 +11,7 @@ export default class Movie extends Component {
 
   componentDidMount() {
     // change this line to grab the id passed on the URL
-    const id = 1;
+    const id = this.props.match.params.id;
     this.fetchMovie(id);
   }
 
